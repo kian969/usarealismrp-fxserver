2c8e7d852e2c3c41bd4fd7ee8e13ca657fb434be
Goryholes <aaronjake0191@gmail.com>
2020-05-12 12:18:18 +0100

    New drug trip - not ready yet

---
+++ resources/acidtrip/client.lua
@@ -0,0 +1,265 @@
+local Cubes = {}
+
+local LastPedInteraction = 0
+local LastPedTurn
+local TripInit
+local PedSpawned
+local EvilPed
+
+local TripState = 1
+local TripTimer = 0
+local TripLength = 15
+local TripAlpha = 0
+local TripAdder = 1
+local TripZOff = -20.0
+local TripZAdd = 0.01
+
+DoAcid = function(time)
+  local song = (time and time >= 200000 and 2 or 1)
+  SendNUIMessage({type = "playMusic", song = song})
+
+  InitCubes()
+
+  local step = 0
+  local timer = GetGameTimer() 
+  local ped = GetPlayerPed(-1)
+  local lastPos = GetEntityCoords(ped)
+
+  while GetGameTimer() - timer < time do
+    local plyPos = GetEntityCoords(GetPlayerPed(-1))
+    local dist = GetVecDist(lastPos,plyPos)
+    if dist > 1.0 then
+      step = step + 1
+      if step == 5 then
+        step = 0
+        local dir = (lastPos - plyPos)
+        local vel = GetEntityVelocity(GetPlayerPed(-1))
+        SetEntityCoordsNoOffset(GetPlayerPed(-1),plyPos.x + dir.x, plyPos.y + dir.y,plyPos.z)
+        ForcePedMotionState(GetPlayerPed(-1), -1115154469, 1, 1, 0)
+        SetEntityVelocity(GetPlayerPed(-1), vel.x,vel.y,vel.z)
+      end
+      lastPos = GetEntityCoords(GetPlayerPed(-1))
+    end
+
+    DrawToons()
+    DrawCubes()
+
+    if TripInit and not PedSpawned then 
+      PedSpawned = true
+      Citizen.CreateThread(InitPed)
+    end
+    Wait(0)
+  end
+
+  ClearTimecycleModifier()
+  ShakeGameplayCam('DRUNK_SHAKE', 0.0)  
+  SetPedMotionBlur(GetPlayerPed(-1), false)
+
+  SetEntityAsMissionEntity(EvilPed,true,true)
+  DeleteEntity(EvilPed)
+
+  SendNUIMessage({type = "stopMusic"})
+
+  Cubes = {}
+
+  LastPedInteraction = 0
+  LastPedTurn = nil
+  TripInit = nil
+  PedSpawned = nil
+  EvilPed = nil
+end
+
+InitPed = function()
+  local plyPed = GetPlayerPed(-1)
+  local pos = GetEntityCoords(plyPed)
+
+  local randomAlt     = math.random(0,359)
+  local randomDist    = math.random(50,80)
+  local spawnPos      = pos + PointOnSphere(0.0,randomAlt,randomDist)
+
+  while World3dToScreen2d(spawnPos.x,spawnPos.y,spawnPos.z) and not IsPointOnRoad(spawnPos.x,spawnPos.y,spawnPos.z) do 
+    randomAlt   = math.random(0,359)
+    randomDist  = math.random(50,80)
+    spawnPos    = GetEntityCoords(GetPlayerPed(-1)) + PointOnSphere(0.0,randomAlt,randomSphere)
+    Citizen.Wait(0)
+  end 
+
+  EvilPed = ClonePed(plyPed, GetEntityHeading(plyPed), false, false)
+  Wait(10)
+  SetEntityCoordsNoOffset(EvilPed, spawnPos.x,spawnPos.y,spawnPos.z + 1.0)
+  SetPedComponentVariation(EvilPed, 1, 60, 0, 0, 0)
+
+  SetEntityInvincible(EvilPed,true)
+  SetBlockingOfNonTemporaryEvents(EvilPed,true)
+
+  TrackEnt()
+end
+
+TrackEnt = function()
+  while true do
+    local dist = GetVecDist(GetEntityCoords(GetPlayerPed(-1)), GetEntityCoords(EvilPed))
+    if dist > 5.0 then
+      TaskGoToEntity(EvilPed, GetPlayerPed(-1), -1, 4.0, 100.0, 1073741824, 0)
+      Wait(1000)
+    else       
+      if not IsTaskMoveNetworkActive(EvilPed) then
+        RequestAnimDict("anim@mp_point")
+        while not HasAnimDictLoaded("anim@mp_point") do Wait(0); end
+        TaskMoveNetworkByName(EvilPed, "task_mp_pointing", 0.5, 0, "anim@mp_point", 24)
+        SetPedCurrentWeaponVisible(EvilPed, 0, 1, 1, 1)
+        SetPedConfigFlag(EvilPed, 36, 1)
+      end
+
+      if not LastPedTurn or (GetGameTimer() - LastPedTurn) > 1000 then
+        LastPedTurn = GetGameTimer()
+        TaskTurnPedToFaceEntity(EvilPed, GetPlayerPed(-1), -1)
+      end
+
+      SetTaskMoveNetworkSignalFloat (EvilPed, "Pitch",          0.4)
+      SetTaskMoveNetworkSignalFloat (EvilPed, "Heading",        0.5)
+      SetTaskMoveNetworkSignalBool  (EvilPed, "isBlocked",      false)
+      SetTaskMoveNetworkSignalBool  (EvilPed, "isFirstPerson",  false)
+
+      if IsPedRagdoll(EvilPed) then
+        while IsPedRagdoll(EvilPed) do Wait(0); end
+        ClearPedTasksImmediately(EvilPed)
+        Wait(10)
+      end
+      Wait(0)
+    end
+  end
+end
+
+InitCubes = function()
+  for i=1,50,1 do
+    local r = math.random(5,255)
+    local g = math.random(5,255)
+    local b = math.random(5,255)
+    local a = math.random(50,100)
+
+    local x = math.random(1,180)
+    local y = math.random(1,359)
+    local z = math.random(15,35)
+
+    Cubes[i] = {pos=PointOnSphere(x,y,z),points={x=x,y=y,z=z},col={r=r, g=g, b=b, a=a}}
+  end  
+
+  ShakeGameplayCam('DRUNK_SHAKE', 0.0) 
+  SetTimecycleModifierStrength(0.0) 
+  SetTimecycleModifier("BikerFilter")
+  SetPedMotionBlur(GetPlayerPed(-1), true)
+
+  local counter = 4000
+  local tick = 0
+  while tick < counter do
+    tick = tick + 1
+    local plyPos = GetEntityCoords(GetPlayerPed(-1))
+    local adder = 0.1 * (tick/40)
+    SetTimecycleModifierStrength(math.min(0.1 * (tick/(counter/40)),1.5))
+    ShakeGameplayCam('DRUNK_SHAKE', math.min(0.1 * (tick/(counter/40)),1.5))  
+    for k,v in pairs(Cubes) do
+      local pos = plyPos + v.pos
+      DrawBox(pos.x+adder,pos.y+adder,pos.z+adder,pos.x-adder,pos.y-adder,pos.z-adder, v.col.r,v.col.g,v.col.g,v.col.a)
+      local points = {x=v.points.x+0.1,y=v.points.y+0.1,z=v.points.z}
+      Cubes[k].points = points
+      Cubes[k].pos = PointOnSphere(points.x,points.y,points.z)
+    end
+    Wait(0)
+  end
+end
+
+DrawCubes = function()
+  local position = GetEntityCoords(GetPlayerPed(-1))
+  local adder = 10
+  for k,v in pairs(Cubes) do
+    local addX = 0.1
+    local addY = 0.1
+
+    if k%4 == 0 then
+      addY = -0.1
+    elseif k%3 == 0 then
+      addX = -0.1
+    elseif k%2 == 0 then
+      addX = -0.1
+      addY = -0.1
+    end
+
+    local pos = position + v.pos
+    DrawBox(pos.x+adder,pos.y+adder,pos.z+adder,pos.x-adder,pos.y-adder,pos.z-adder, v.col.r,v.col.g,v.col.g,v.col.a)
+    local points = {x=v.points.x+addX,y=v.points.y+addY,z=v.points.z}
+    Cubes[k].points = points
+    Cubes[k].pos = PointOnSphere(points.x,points.y,points.z)
+  end
+end
+
+DrawToons = function()
+  local plyPed = GetPlayerPed(-1)
+  local plyPos = GetEntityCoords(plyPed)
+
+  local infront = vector3(plyPos.x+35.0, plyPos.y-8.0,plyPos.z)
+  local behind  = vector3(plyPos.x-35.0, plyPos.y-8.0,plyPos.z)
+
+  if (GetGameTimer() - TripTimer) > 1000 then
+    TripTimer = GetGameTimer()
+    TripState = TripState + TripAdder
+
+    if TripState then
+      TripAdder = -1
+      TripState = 2
+    elseif TripState <= 0 then
+      TripState = 2
+      TripAdder = 1
+    end
+  end
+
+  DrawTrip(infront)
+  DrawTrip(behind)
+end
+
+DrawTrip = function(loc)
+  local height = 0
+  local width = 0
+
+  if TripZOff < 0.0 then TripZOff = TripZOff + TripZAdd; end
+  for k = #Trip.bits[TripState],1,-1 do
+    local v = Trip.bits[TripState][k]
+    local pos = vector3(loc.x,loc.y+width,loc.z+height)
+    local col = Trip.cols[v]    
+
+    if TripAlpha < 255 then
+      if not TripInit then TripInit = true; end
+      TripAlpha = TripAlpha + 0.001
+    end
+
+    if v ~= 133 then
+      DrawBox(pos.x+0.5,pos.y+0.5,pos.z+0.5 + TripZOff, pos.x-0.5,pos.y-0.5,pos.z-0.5 + TripZOff, col.r,col.g,col.b,math.floor(TripAlpha))
+    end
+
+    width = width + 1
+    if width > TripLength then
+      width = 0
+      height = height + 1
+    end
+  end
+end
+
+GetVecDist = function(v1,v2)
+  if not v1 or not v2 or not v1.x or not v2.x then return 0; end
+  return math.sqrt(  ( (v1.x or 0) - (v2.x or 0) )*(  (v1.x or 0) - (v2.x or 0) )+( (v1.y or 0) - (v2.y or 0) )*( (v1.y or 0) - (v2.y or 0) )+( (v1.z or 0) - (v2.z or 0) )*( (v1.z or 0) - (v2.z or 0) )  )
+end
+
+PointOnSphere = function(alt,azu,radius,orgX,orgY,orgZ)
+  local toradians = 0.017453292384744
+  alt,azu,radius,orgX,orgY,orgZ = ( tonumber(alt or 0) or 0 ) * toradians, ( tonumber(azu or 0) or 0 ) * toradians, tonumber(radius or 0) or 0, tonumber(orgX or 0) or 0, tonumber(orgY or 0) or 0, tonumber(orgZ or 0) or 0
+  if      vector3
+  then
+      return
+      vector3(
+           orgX + radius * math.sin( azu ) * math.cos( alt ),
+           orgY + radius * math.cos( azu ) * math.cos( alt ),
+           orgZ + radius * math.sin( alt )
+      )
+  end
+end
+
+RegisterCommand("acid", function(...) DoAcid(20000); end)
