d6776306ffd0c5b22ed67351100beec511730b17
Goryholes <aaronjake0191@gmail.com>
2020-05-20 20:30:08 +0100

    Scuba tank

--- resources/usa_scuba/client/main.lua
+++ resources/usa_scuba/client/main.lua
@@ -1,19 +1,38 @@
+local AIR_TANK_CONSUMPTION = math.random(30, 120)
+
 Citizen.CreateThread(function()
     while true do
         Wait(0)
         local player = PlayerPedId()
         --if IsControlJustPressed(0, 86) then
-            SetPedScubaGearVariation(player)
-            SetPedMaxTimeUnderwater(player, 360.0)
-            if IsPedSwimmingUnderWater(player) == 1 then
-                TriggerServerEvent('scuba:checkItems')
-            end
+        if IsPedSwimmingUnderWater(player) == 1 then
+            TriggerServerEvent('scuba:checkItems')
+        end
         --end
     end
 end)
 
 RegisterNetEvent('scuba:useAirTank')
 AddEventHandler('scuba:useAirTank', function()
-    local test = GetPlayerUnderwaterTimeRemaining(PlayerId())
-    exports.globals:notify(test)
+    local player = PlayerPedId()
+    SetPedScubaGearVariation(player)
+    SetPedMaxTimeUnderwater(player, 100.0 * 2)  -- 10 mins air supply
+    local oxygenRemaining = GetPlayerUnderwaterTimeRemaining(PlayerId())
+    if oxygenRemaining <= AIR_TANK_CONSUMPTION then
+        TriggerServerEvent('scuba:removeTank')
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        local oxygenRemaining = GetPlayerUnderwaterTimeRemaining(PlayerId())
+
+        print(oxygenRemaining)
+        if oxygenRemaining == 100 then
+            exports.globals:notify('Oxygen tank is at 50%')
+        elseif oxygenRemaining == 50 then
+            exports.globals.notify('Oxygen Running low, return to surface')
+        end
+    end
 end)
--- resources/usa_scuba/server/main.lua
+++ resources/usa_scuba/server/main.lua
@@ -3,7 +3,16 @@ local has_tank = nil
 RegisterServerEvent('scuba:checkItems')
 AddEventHandler('scuba:checkItems', function()
     local char = exports["usa-characters"]:GetCharacter(source)
+
     if char.hasItem('Air Tank') then
-        TriggerClientEvent('scuba:useAirTank')
+        TriggerClientEvent('scuba:useAirTank', source)
+    end
+end)
+
+RegisterServerEvent('scuba:removeTank')
+AddEventHandler('scuba:removeTank', function()
+    local char = exports["usa-characters"]:GetCharacter(source)
+    if char.hasItem("Air Tank") then
+        char.removeItem("Air Tank")
     end
 end)
