8c866f007077bd77758fe685247f9370d35c8cc9
minipunch <joshyolles@gmail.com>
2020-06-21 02:32:38 -0700

    initial commit, see notes for todo

---
+++ resources/_nitro/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+name 'sw-nitro'
+description 'The most advanced nitro system for FiveM'
+author 'Deltanic - https://github.com/Deltanic/'
+url 'https://github.com/swcfx/sw-nitro'
+
+client_script 'client/utils.lua'
+client_script 'client/fuel.lua'
+client_script 'client/ptfx.lua'
+client_script 'client/boost.lua'
+client_script 'client/purge.lua'
+client_script 'client/trails.lua'
+client_script 'client/main.lua'
+
+server_script 'server/main.lua'
