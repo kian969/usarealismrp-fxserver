39c7f6873dc77a1ea8833049936f046ad2133427
minipunch <joshyolles@gmail.com>
2022-09-14 17:55:46 -0700

    911 calls working

--- resources/npwd/framework.lua
+++ resources/npwd/framework.lua
@@ -1,11 +1,18 @@
 AddEventHandler("character:loaded", function(char)
+    if not char.get("phoneNumber") then
+        char.set("phoneNumber", exports.npwd:generatePhoneNumber())
+    end
     exports.npwd:newPlayer({
         source = char.get("source"),
-        phoneNumber = "949351544" .. tostring(math.random(0, 9)),
+        phoneNumber = char.get("phoneNumber"),
         identifier = char.get("_id"),
         firstname = char.get("name").first,
         lastname = char.get("name").last
     })
 end)
 
--- todo: call unload player event when /swap is used
+exports.npwd:onMessage('911', function(ctx)
+    local charPhoneNumber = exports["usa-characters"]:GetCharacter(ctx.source).get("phoneNumber")
+    local coords = GetEntityCoords(GetPlayerPed(ctx.source))
+    TriggerEvent('911:call', coords.x, coords.y, coords.z, "[" .. charPhoneNumber .. "] " .. ctx.data.message, "Call for service")
+end)
