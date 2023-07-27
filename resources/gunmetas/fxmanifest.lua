9a3ce67ffd7b043b1d5ed72d0acc9420b5a400ec
Chang <77425784+ChangyBoi@users.noreply.github.com>
2023-07-27 21:18:50 +0200

    Weapon Range Changes (#2192)

---
+++ resources/gunmetas/fxmanifest.lua
@@ -0,0 +1,51 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+description 'Weapon Meta Files'
+
+files {
+	'weaponanimations.meta',
+	'weapon_ceramicpistol.meta',
+	'weapon_militaryrifle.meta',
+	'weaponbullpuprifle.meta',
+	'weaponcombatpdw.meta',
+	'weaponcompactrifle.meta',
+	'weapongusenberg.meta',
+	'weaponheavypistol.meta',
+	'weaponmachinepistol.meta',
+	'weaponminismg.meta',
+	'weapons_assaultrifle_mk2.meta',
+	'weapons_bullpuprifle_mk2.meta',
+	'weapons_carbinerifle_mk2.meta',
+	'weapons_pistol_mk2.meta',
+	'weapons_pistol50.meta',
+	'weapons_smg_mk2.meta',
+	'weapons_snspistol_mk2.meta',
+	'weapons_specialcarbine_mk2.meta',
+	'weaponsnspistol.meta',
+	'weaponspecialcarbine.meta',
+	'weaponsmg.meta'
+}
+
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_ceramicpistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_militaryrifle.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponbullpuprifle.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponcombatpdw.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponcompactrifle.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapongusenberg.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponheavypistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponmachinepistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponminismg.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_assaultrifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_bullpuprifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_carbinerifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_pistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_pistol50.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_snspistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_smg_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons_specialcarbine_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponsnspistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponspecialcarbine.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponsmg.meta'
