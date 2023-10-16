b165940fe7f5189b1c6ab70c817388d0dc3d3f97
Savage912 <55938321+Savage912-Official@users.noreply.github.com>
2023-10-16 02:39:03 -0400

    update: Event Planners Temp Blips CMD (#2089)
    * update: Event Planners Temp Blips CMD
    
    Temporary Blips for event planners.
    
    closes #601
    
    * update: Event Planners Temp Blips CMD
    
    * Update sv_eventPlanners.lua
    
    * Update __resource.lua
    
    * Update cl_eventPlanners.lua
    
    * Sync blips to server & client
    
    Savage asked if I could work on this for him.
    
    * Use GetEntityCoords rather than manual input
    
    * don't show blip added msg on load
    
    ---------
    
    Co-authored-by: Jay <123783637+K7AKEN@users.noreply.github.com>
    Co-authored-by: minipunch <joshyolles@gmail.com>

--- resources/usa_eventPlanners/sv_eventPlanners.lua
+++ resources/usa_eventPlanners/sv_eventPlanners.lua
@@ -2,6 +2,8 @@ local DISCORD_WEBHOOK_URL = GetConvar("event-team-webhook", "")
 
 local currentlySignedInPlayers = {}
 
+local createdBlips = {}
+
 RegisterNetEvent("eventPlanner:toggleDuty")
 AddEventHandler("eventPlanner:toggleDuty", function()
     local c = exports["usa-characters"]:GetCharacter(source)
@@ -32,5 +34,42 @@ AddEventHandler("playerDropped", function(reason)
     if currentlySignedInPlayers[source] then
         exports.globals:SendDiscordLog(DISCORD_WEBHOOK_URL, "`" .. currentlySignedInPlayers[source].name .. "` [" .. currentlySignedInPlayers[source].ident .. "] has `signed out (disconnected)`.")
         currentlySignedInPlayers[source] = nil
+    end
+end)
+
+-- Temporary Blips --
+TriggerEvent('es:addJobCommand', 'addblip', {'eventPlanner'}, function(source, args, char)
+    TriggerClientEvent('usa_eventPlanners:client:blipDialog', source)
+end, {
+    help = "Add Temporary Event Blip To Map!",
+})
+
+RegisterNetEvent('usa_eventPlanners:server:addBlip')
+AddEventHandler('usa_eventPlanners:server:addBlip', function(blipData)
+
+    print(string.format('A blip has been set by ID: %s, with the name of %s for %smins!', tostring(source), blipData.name, blipData.duration / 60000))
+
+    blipData.duration = blipData.duration + GetGameTimer() -- (Client & server time values are for some reason different)
+    table.insert(createdBlips, blipData)
+    TriggerClientEvent('usa_eventPlanners:client:addBlip', -1, {blipData}) -- Adds blip to others in game (Sync)
+
+end)
+
+RegisterNetEvent('usa_eventPlanners:server:syncBlips')
+AddEventHandler('usa_eventPlanners:server:syncBlips', function()
+    TriggerClientEvent('usa_eventPlanners:client:addBlip', source, createdBlips) -- ensures newly loaded clients can see the blip (Sync)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        local currentTime = GetGameTimer()
+        for i, blipToRemove in pairs(createdBlips) do
+            if currentTime >= blipToRemove.duration then
+                print(currentTime, blipToRemove.duration)
+                table.remove(createdBlips, i)
+                TriggerClientEvent('usa_eventPlanners:client:removeBlip', -1, blipToRemove.name)
+            end
+        end
     end
 end)
